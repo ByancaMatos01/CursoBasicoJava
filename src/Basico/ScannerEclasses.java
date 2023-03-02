7ccf53aa79c849c7d96f0d7550bbd07f5408e98f
ByancaMatos01 <122841376+ByancaMatos01@users.noreply.github.com>
2023-03-02 10:47:45 -0300

    Java Básico Curso

---
+++ src/Basico/ScannerEclasses.java
@@ -0,0 +1,10 @@
+package Basico;
+
+public class ScannerEclasses {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Basico/tipos.java
@@ -0,0 +1,5 @@
+package Basico;
+
+public class tipos {
+
+}
