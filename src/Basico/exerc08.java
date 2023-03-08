21ad460e7225735226dfd1a154f76fa19875597d
ByancaMatos01 <122841376+ByancaMatos01@users.noreply.github.com>
2023-03-08 09:10:31 -0300

    java3

--- src/Basico/exerc08.java
+++ src/Basico/exerc08.java
@@ -1,9 +1,33 @@
 package Basico;
 
+import java.util.Scanner;
+
+/*
+ * Faça um Programa que pergunte quanto você ganha por hora e o
+	número de horas trabalhadas no mês. Calcule e mostre o total do seu
+	salário no referido mês.
+ */
 public class exerc08 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int hr, nrht;
+		double totalmes;
+		
+		Scanner leia=new Scanner(System.in);
+		
+		System.out.println("Quanto você ganha por  hora: ");
+		hr=leia.nextInt();
+		
+		System.out.println("Qual é o numeros de horas trabalhadas no mês: ");
+		nrht=leia.nextInt();
+		
+		totalmes=hr*nrht;
+		
+		System.out.println("O total de horas é "+ totalmes);
+		
+		
+		
+		
 
 	}
 
