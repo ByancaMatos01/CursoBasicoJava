5d58de620f6887cddbb4b22e9854c033e0104282
ByancaMatos01 <122841376+ByancaMatos01@users.noreply.github.com>
2023-03-08 09:46:29 -0300

    java10

--- src/Basico/Farenheit.java
+++ src/Basico/Farenheit.java
@@ -1,9 +1,28 @@
 package Basico;
 
+import java.util.Scanner;
+
+/*
+ * Faça um Programa que peça a temperatura em graus Farenheit, 
+	transforme e mostre a temperatura em graus Celsius.
+	o C = (5 * (F-32) / 9).
+ */
 public class Farenheit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner leia=new Scanner(System.in);
+		double c,f;
+		System.out.println("digite a temperatura em Fareheint");
+		f=leia.nextInt();
+		
+		c=((5*(f-32)/9));
+		
+		System.out.println("a temperatura em fareheint para ceulsius  "+ c + "c");
+		
+		
+		
+		
+		
 
 	}
 
---
+++ src/Basico/exerc10.java
@@ -0,0 +1,19 @@
+package Basico;
+
+import java.util.Scanner;
+
+/*
+ * Faça um Programa que peça a temperatura em graus Celsius, 
+	transforme e mostre em graus Farenheit.
+ */
+public class exerc10 {
+
+	public static void main(String[] args) {
+		int c,f;
+		Scanner leia=new Scanner(System.in);
+		
+		
+
+	}
+
+}
